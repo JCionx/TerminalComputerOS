b2e42f931589c3ef3b9cee600f4d66148501451b
JCionx <92257741+JCionx@users.noreply.github.com>
2021-12-25 21:54:10 +0000

    Add files via upload

---
+++ terminalcomputerOS.py
@@ -0,0 +1,96 @@
+
+import os
+import time
+import webbrowser
+from datetime import datetime
+from termcolor import colored, cprint
+
+os.system("cls")
+
+print(" _________")
+print("|\\________\\")
+print("\\|___  ___|")
+print("   || |")
+print("   || |")
+print("   \\|_| E R M I N A L    C O M P U T E R")
+
+user = input()
+
+cprint(" Press enter to begin installation.", "grey", "on_white")
+
+user = input()
+
+cprint("\n Choose a location for your 'TERMINAL COMPUTER' system instalaltion.", "grey", "on_white")
+location = input("C:\\")
+
+percent = 0
+
+while not percent >= 100:
+    os.system("cls")
+    cprint("\n Installing. Please don't unplug your device nor unplug the media installation device.", "grey", "on_white")
+    print(percent, "%")
+    time.sleep(.2)
+    percent += 2
+
+os.system("cls")
+
+cprint("\n Installation complete! Restarting...", "grey", "on_white")
+
+time.sleep(2)
+
+os.system("cls")
+
+cprint("\n" * 4 + " " * 45 + "...." +  " " * 45, "grey", "on_white")
+cprint(" " * 30 + "T E R M I N A L    C O M P U T E R" + " " * 30, "grey", "on_white")
+cprint(" " * 45 + "...." +  " " * 45, "grey", "on_white")
+
+time.sleep(4)
+
+os.system("cls")
+
+cprint(" Welcome to Terminal Computer! \n use /help to help. ", "grey", "on_white")
+
+while True:
+
+    command = input("\n$")
+    if str(command) == "/help":
+        cprint("\n /help - Shows this list\n /files - Shows all files on your hard drive\n /open [file.name] - Opens a file\n /time - Shows the current time (h,m,s)", "grey", "on_white")
+    elif str(command) == "/files":
+        cprint("\n GoogleChrome.tcex\n random_text_file.docz\n shutdown.py", "grey", "on_white")
+    elif str(command) == "/open GoogleChrome.tcex":
+        cprint("\nAre you sure you want to execute this program? [y, n]", "grey", "on_white")
+        if input("\n") == "y":
+            webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    elif str(command) == "/open random_text_file.docz":
+        print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣶⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⠿⠟⠛⠻⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣆⣀⣀⠀⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠻⣿⣿⣿⠅⠛⠋⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢼⣿⣿⣿⣃⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣟⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣛⣛⣫⡄⠀⢸⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡆⠸⣿⣿⣿⡷⠂⠨⣿⣿⣿⣿⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⡇⢀⣿⡿⠋⠁⢀⡶⠪⣉⢸⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿⣷⣿⣿⣷⣦⡙⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣇⢸⣿⣿⣿⣿⣿⣷⣦⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+'NEVER GONNA GIVE YOU UP
+NEVER GONNA LET YOU DOWN'
+by Rick Astley""")
+    elif str(command) == "/open shutdown.py":
+        cprint("\n Shutting down.", "grey", "on_white")
+        time.sleep(3)
+        os.system("cls")
+        break
+    elif str(command) == "/time":
+        t = datetime.now()
+        timenow = " " + str(t.hour) + ":" + str(t.minute) + ":" + str(t.second) + " "
+        cprint(timenow, "grey", "on_white")
+    else:
+        cprint("\n Command not found. ", "grey", "on_white")
